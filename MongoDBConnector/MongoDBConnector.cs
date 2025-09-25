0eb31590ab9253b351d841c651531658af8366ef
NARodman <ninaalanier8@gmail.com>
2025-09-25 14:21:00 -0400

    Chore: add Ping method for MongoDBConnector

--- MongoDBConnector/MongoDBConnector.cs
+++ MongoDBConnector/MongoDBConnector.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoDBConnector
 {
@@ -10,7 +11,23 @@ namespace MongoDBConnector
         {
             _client = new MongoClient(connectionString);
         }
+
+        public bool Ping() {
+            try
+            {
+                var database = _client.GetDatabase("admin");
+                var command = new BsonDocument("ping", 1);
+                var result = database.RunCommand<BsonDocument>(command);
+                return result.Contains("ok") && result["ok"] == 1;
+            }
+            catch
+            {
+                return false;
+        }
     }
+}
+
+    
 }
 
 
